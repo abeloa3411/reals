ded899e4277dc6c0a70cc82d3d6027e47b99a584
abel wanyonyi <abelw34@gmail.com>
2023-12-29 14:15:02 +0300

    video captions and prof pic

--- app/src/main/java/com/example/reals/adapter/VideoListAdapter.kt
+++ app/src/main/java/com/example/reals/adapter/VideoListAdapter.kt
@@ -4,10 +4,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.reals.R
 import com.example.reals.databinding.VideoItemRowBinding
+import com.example.reals.model.UserModel
 import com.example.reals.model.VideoModel
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class VideoListAdapter(
 options: FirestoreRecyclerOptions<VideoModel>
@@ -16,9 +22,28 @@ options: FirestoreRecyclerOptions<VideoModel>
     inner class VideoViewHolder(private val binding: VideoItemRowBinding) : RecyclerView.ViewHolder(binding.root){
 
         fun bindVideo(videoModel:VideoModel){
+            //bind user data
+            Firebase.firestore.collection("users").document(videoModel.uploaderId).get().addOnSuccessListener {
+                val userModel = it?.toObject(UserModel::class.java)
+                userModel?.apply{
+                    binding.usernameView.text = username
+
+                    //bind profile pic
+                    Glide.with(binding.profileIcon).load(profilePic).circleCrop().apply(
+                        RequestOptions().placeholder(R.drawable.baseline_person_24)
+                    ).into(binding.profileIcon)
+                }
+            }
+
+            binding.captionView.text = videoModel.title
+            binding.progressBar.visibility = View.VISIBLE
+
+
+            //bind video
             binding.videoView?.apply{
                 setVideoPath(videoModel.url)
                 setOnPreparedListener{
+                    binding.progressBar.visibility = View.GONE
                     it.start()
                     it.isLooping = true
                 }
